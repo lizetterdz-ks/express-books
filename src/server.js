c9a1ccdc89f1303c849a7cd89e5f31727bfd9d26
lizetterdz <71050320+lizetterdz@users.noreply.github.com>
2021-10-22 16:02:25 -0500

    commit on main

--- src/server.js
+++ src/server.js
@@ -19,5 +19,5 @@ app.use((req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Book database app listening at http://localhost:${PORT}`)
 });
